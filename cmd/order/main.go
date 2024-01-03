6df05ad84ce19ac9260fef452cc886b1eb46a8e2
Rodrigo Mota <rodrigomotap.campos@gmail.com>
2024-01-03 17:08:12 -0300

    .

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -1,38 +1,39 @@
 package order
 
 import (
-	"database/sql";
-	"fmt";
-	"strconv";
-	"github.com/motA1215/goLang/internal/infra/database";
-	"github.com/motA1215/goLang/internal/usecase";
-	_ "github.com/mattn/go-sqlite3";
-);
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/motA1215/goLang/internal/infra/database"
+	"github.com/motA1215/goLang/internal/usecase"
+)
 
 func main() {
-	db, err := sql.Open("sqlite3", "db.sqlite3");
-	if err != nil{
-		panic(err);	
+	db, err := sql.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
 	}
 
-	uc := usecase.NewCalculateFinalPrice(database.NewOrderRepository(db));
+	uc := usecase.NewCalculateFinalPrice(database.NewOrderRepository(db))
 
 	input := usecase.OrderInput{
-		ID: "1235",
+		ID:    "1235",
 		Price: 10.0,
-		Taxa: 1.0,
+		Taxa:  1.0,
 	}
 
-	output, err := uc.Execute(input);
-	if err != nil{
-		panic(err);	
+	output, err := uc.Execute(input)
+	if err != nil {
+		panic(err)
 	}
 
-	rowTotals, err := uc.GetTotalRowsTable();
-	if err != nil{
-		panic(err);
+	rowTotals, err := uc.GetTotalRowsTable()
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Println(output);
-	fmt.Println("Quantidade de preços finais inseridos na tabela de orders: " + strconv.Itoa(rowTotals));
+	fmt.Println(output)
+	fmt.Println("Quantidade de preços finais inseridos na tabela de orders: " + strconv.Itoa(rowTotals))
 }
